d425849009d8b799ea4e62839e9c4ab0246ba586
James Ding <git@twango.dev>
2026-05-29 23:50:48 -0700

    refactor(plugin): remove redundant comments for self-explanatory code

--- internal/plugin/error.go
+++ internal/plugin/error.go
@@ -6,11 +6,7 @@ import (
 	"fmt"
 )
 
-// ErrorFragment produces a fragment that renders a graceful "this plugin failed"
-// box. Used when a plugin's Fetch or Render returns an error and the engine is
-// not in strict mode.
 func ErrorFragment(pluginName string, err error) Fragment {
-	// Use encoding/xml escaping for SVG-attribute safety.
 	name := xmlEscape(pluginName)
 	msg := xmlEscape(err.Error())
 	body := fmt.Sprintf(`<g class="plugin-error" data-plugin="%s">
@@ -20,9 +16,6 @@ func ErrorFragment(pluginName string, err error) Fragment {
 	return Fragment{Body: body, Width: 440, Height: 40}
 }
 
-// xmlEscape returns s with characters that are special in XML/SVG (such as
-// '<', '>', '&', and quotes) replaced by their entity references. It is a
-// thin wrapper around encoding/xml's EscapeText.
 func xmlEscape(s string) string {
 	var buf bytes.Buffer
 	_ = xml.EscapeText(&buf, []byte(s))
